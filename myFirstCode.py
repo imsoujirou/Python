747422a2144dcbd29edd253f066dde56fd4a0c4b
imsoujirou <99232659+imsoujirou@users.noreply.github.com>
2023-06-29 23:46:41 +0800

    notes

--- myFirstCode.py
+++ myFirstCode.py
@@ -1,5 +1,11 @@
 # This program says hello and asks for my name
 
+# Notes
+#print is to display 
+#input is to input 
+#len is string length
+#data types string, int, float
+
 print('Hello World!')
 print('What is your name?') # ask for their name
 myName = input()
@@ -9,4 +15,3 @@ print(len(myName))
 print('What is your age?') # ask for age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
-    
