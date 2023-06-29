e32c934ed8e8103682d2bd327ce96c02c7277657
imsoujirou <99232659+imsoujirou@users.noreply.github.com>
2023-06-29 23:32:26 +0800

    edited some functions

--- myFirstCode.py
+++ myFirstCode.py
@@ -4,7 +4,9 @@ print('Hello World!')
 print('What is your name?') # ask for their name
 myName = input()
 print('It is good to meet you, ' + myName)
+print('The length of you name is')
 print(len(myName))
 print('What is your age?') # ask for age
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+    
